932ac2dd0863fad4b531052d12b34a3f700212b9
Carl Lewis <carl@smarter.am>
2018-09-19 17:28:05 +0100

    minor changes

---
+++ app/src/main/java/com/carllewisapps/notes/app/Constants.java
@@ -0,0 +1,6 @@
+package com.carllewisapps.notes.app;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://demo.androidhive.info/";
+}
---
+++ app/src/main/java/com/carllewisapps/notes/utils/PrefUtils.java
@@ -0,0 +1,24 @@
+package com.carllewisapps.notes.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PrefUtils {
+
+    /**
+     * Stores API Key in shared pref to
+     * add it in header part of every retrofit request
+     */
+    public PrefUtils(){
+
+    }
+
+    private static SharedPreferences getSharedPref(Context context) {
+        return context.getSharedPreferences("APP_PREF", Context.MODE_PRIVATE);
+    }
+
+
+    public static String getApiKey(Context context) {
+        return getSharedPref(context).getString("API_KEY", null);
+    }
+}
